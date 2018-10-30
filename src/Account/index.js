a2dcef18fa4b5cdb9bce0399e6f14712c061fcbb
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-29 20:26:00 -0500

    added state for balance

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      balance: 0
+    }
+  }
   render() {
     return (
       <div className="account">
