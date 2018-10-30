07c65e25679b5be17bb2e1d99b45ad84cafc486c
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-29 21:05:42 -0500

    can now change amount in checking and savings

--- src/Account/index.js
+++ src/Account/index.js
@@ -8,13 +8,20 @@ class Account extends Component {
       balance: 0
     }
   }
+  handleDeposit = (e) => {
+    console.log(this.inputBox.value)
+    e.preventDefault()
+    this.setState({
+      balance: parseInt(this.inputBox.value)
+    })
+  }
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
+        <div className="balance">{this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
+        <input onClick={this.handleDeposit} type="button" value="Deposit" />
         <input type="button" value="Withdraw" />
       </div>
     )
